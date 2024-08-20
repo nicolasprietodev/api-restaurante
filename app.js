76db63d98556930d3c69ae4d4a4793be934309d6
NicolasPrietoPinzonADSO <nicolasprietopinzon5@gmail.com>
2024-08-19 23:01:43 -0500

    se arreglo problema de cors y se dejo configurado para variables de entorno del despliegue

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 import express, { json } from "express";
 import { createRestauranteRouter } from "./routes/restauranteRouter.js";
 import { corsMiddleware } from "./middleware/cors.js";
+import 'dotenv/config'
 
 export const createApp = ({ restauranteModel }) => {
   const app = express();
-  const PORT = process.env.PORT ?? 1235;
-
+  
   app.disable("x-powered-by");
   app.use(json());
-  app.use(corsMiddleware);
+  app.use(corsMiddleware());
   app.use("/restaurante", createRestauranteRouter({ restauranteModel }));
   console.log("estamos en app");
+  const PORT = process.env.PORT ?? 1235;
 
   app.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto http://localhost:${PORT}`);
--- middleware/cors.js
+++ middleware/cors.js
@@ -1,6 +1,6 @@
 import cors from 'cors'
 const ACCEPTED_ORIGINS = [
-  'http://localhost:1235',
+  'http://localhost:1235'
 ]
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
